0533dd63cc85c5a5725460cf491d3444b147ca65
oycxyd <yuchen.xiang11@imperial.ac.uk>
2023-09-26 16:42:39 +0100

    Update raw2mat.m

--- cwtpp/util/raw2mat.m
+++ cwtpp/util/raw2mat.m
@@ -5,11 +5,14 @@ function [raw_specs, dims] = raw2mat(filename)
     numS = calllib('MassLynxRaw','getScansInFunction',p2,1);
 
     raw_specs = {};xy = {};
-    for n = 1:numS
+    counter1 = parfor_wait(numS, 'Waitbar', true);
+    parfor n = 1:numS
+        counter1.Send;
         [mz, spectrum,xy{n}] = readraw2spec(filename,n);
         raw_specs{n} = cat(1,mz,spectrum);
     end
-
+    counter1.Destroy
+    
     xy = cell2mat(xy);
     pixel = abs(xy(1)-xy(3));
     x_len = round(abs(xy(end-1)-xy(1))/pixel)+1;
@@ -17,7 +20,7 @@ function [raw_specs, dims] = raw2mat(filename)
 
     dims = [x_len,y_len];
     
-    if length(specs) ~= x_len.*y_len
+    if length(raw_specs) ~= x_len*y_len
         disp('Warning! The pixel dimension according to the metadata does not match the file size.')
     end
 end
