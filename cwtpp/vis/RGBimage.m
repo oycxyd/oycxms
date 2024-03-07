198f12a5af73ee75c5f2ed7b25a1cf701b260e66
oycxyd <yuchen.xiang11@imperial.ac.uk>
2024-03-07 09:46:31 +0000

    peakfit

--- cwtpp/vis/RGBimage.m
+++ cwtpp/vis/RGBimage.m
@@ -1,13 +1,12 @@
 function output_images = RGBimage(varargin)
     if nargin < 3
         images = varargin{1};
-        dims = varargin{2};
-%         rgb = cat(3,(images(:,:,1)),(images(:,:,2)),(images(:,:,3)));
+        images = cat(3,mat2gray(images(:,:,1)),mat2gray(images(:,:,2)),mat2gray(images(:,:,3)));%normalise each channel
+        % dims = varargin{2};
         rgb_ind = perms(1:3);
         for i = 1:length(rgb_ind)
             ind = rgb_ind(i,:);
             rgb = cat(3,imadjust(images(:,:,ind(1))),imadjust(images(:,:,ind(2))),imadjust(images(:,:,ind(3))));
-    %         rgb = reshape (rgb, [dims(1) dims(2)]);
             rgb = (rgb-min(rgb(:)))/max(rgb(:));
             rgb = im2uint8(rgb);
             figure,imagesc(rgb);axis image
