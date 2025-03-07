ee5a9df7c150fe81c4bbe2b9cc1a59b896c5f86e
J McKenzie <jsmckenzie@users.noreply.github.com>
2025-03-07 15:55:11 +0000

    Quicker normalisation function

--- cwtpp/util/TIC_norm.m
+++ cwtpp/util/TIC_norm.m
@@ -1,12 +1,39 @@
 function [specs_norm] = TIC_norm(specs_input)
-    counter1 = parfor_wait(size(specs_input,1), 'Waitbar', true);
-    parfor i = 1:size(specs_input,1)
-        counter1.Send;
-        if sum(specs_input(i,:))~= 0
-            specs_norm(i,:) = specs_input(i,:)/sum(specs_input(i,:));
-        else
-            specs_norm(i,:) = zeros(1,length(specs_input(i,:)));
-        end
+% Original also highly bloated.
+
+% This is quicker by typically >1000 fold (typically ~ 3 seconds), less
+% when the wait bar is turned off
+% tic;
+specs_norm = bsxfun(@rdivide,specs_input,sum(specs_input,2));
+% toc;
+
+% Benchmark cmparison below
+% tic;
+% [a] = original(specs_input);
+% toc;
+% 
+% 
+% df = bsxfun(@minus,specs_norm,a);
+% df = bsxfun(@rdivide,df,specs_input) * 100;
+% 
+% disp(['Largest difference is ' sprintf('%0.6f',max(df(:))) '%']);
+
+
+end
+
+
+function [specs_norm] = original(specs_input)
+
+%counter1 = parfor_wait(size(specs_input,1), 'Waitbar', true);
+parfor i = 1:size(specs_input,1)
+    %counter1.Send;
+    if sum(specs_input(i,:))~= 0
+        specs_norm(i,:) = specs_input(i,:)/sum(specs_input(i,:));
+    else
+        specs_norm(i,:) = zeros(1,length(specs_input(i,:)));
     end
-    counter1.Destroy
+end
+
+%counter1.Destroy
+
 end
