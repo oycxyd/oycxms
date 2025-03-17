0fb2872ac1c5d61f1eb413243646ca5cd183ebe0
oycxyd <yuchen.xiang11@imperial.ac.uk>
2025-03-17 14:23:30 +0000

    Update msreffind.m

--- cwtpp/util/msreffind.m
+++ cwtpp/util/msreffind.m
@@ -53,13 +53,10 @@ function [mzs_output, ms_references] = msreffind(varargin)
     mz_recal = mzs_recal{1};
 %     mzs_recal = mzs;
     ms_references = [];
-    if strcmp(mode,'workspace')
-         ms_references(:,1) = mz_recal;
-    else 
-        [~,locs] = findpeaks(mean(datasets{1}),'MinPeakProminence',mean(mean(datasets{1}))/3);
-        mz_recal_p = mz_recal(locs);
-        ms_references(:,1) = mz_recal_p;
-    end
+
+    [~,locs] = findpeaks(mean(datasets{1}),'MinPeakProminence',mean(mean(datasets{1}))/3);
+    mz_recal_p = mz_recal(locs);
+    ms_references(:,1) = mz_recal_p;
    
 
 
