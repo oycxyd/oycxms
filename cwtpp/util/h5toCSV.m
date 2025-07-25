d7c5b2c5aaf948ba037c6194c3990b7e1047cda8
ZahraNozari <zahraa.nozari@gmail.com>
2025-07-25 18:46:34 +0200

    mz5 mode added to h5tocsv function

--- cwtpp/util/h5toCSV.m
+++ cwtpp/util/h5toCSV.m
@@ -1,8 +1,23 @@
-function [output] = h5toCSV()
+function [output] = h5toCSV(mode)
+
+arguments
+    mode string = '.raw'
+end
 try
     dname = uigetdir();
     cd (dname);
+
+if strcmp (mode, '.raw')
     filenames=dir('*.raw');
+    h5_suffix = '\datacube_aligned.h5';
+
+elseif strcmp (mode, '.mz5')
+    filenames = dir('*.mz5');
+    h5_suffix = '_aligned.h5';
+
+    else
+        error('invalid mode. Use ''.raw'' or ''.mz5''');
+    end
 
     if exist('metadata.csv','file') > 0
             metadata = readcell('metadata.csv');
@@ -19,8 +34,22 @@ try
     for i = 1:length(filenames)
         disp(i)
         filename = filenames(i).name;
+
+        if strcmp(mode, '.raw')
         id = find(strcmp(metadata(:,2), filename));
-        [dum,~,mz]=h5toMat([filename,'\datacube_aligned.h5']);
+        h5_path = [filename, h5_suffix];
+
+        else % .mz5 mod  
+         id = find(strcmp(metadata(:,2), filename));
+         [~, base_name, ~] = fileparts(filename);
+         h5_path = [base_name, h5_suffix];
+        end
+          if strcmp(mode, '.mz5') && ~exist(h5_path, 'file')
+            continue;
+          end
+
+         [dum,~,mz] = h5toMat(h5_path);
+         
         scan_num = 1;
         for j = 1:length(id)
             num_scans = cell2mat(metadata(id(j),4))-cell2mat(metadata(id(j),3));
@@ -28,28 +57,36 @@ try
             end_scan = cat(1,end_scan,metadata(id(j),4));
             labels = cat(1,labels,char(metadata(id(j),1)));
             % labels2 = cat(1,labels2,char(metadata(id(j),9)));
-            files = cat(1,files,char(metadata(id(j),2)));
+
+
+            if strcmp(mode, '.raw')
+                files = cat(1, files, char(metadata(id(j),2)));
+            else % .mz5 mode - 
+                files = cat(1, files, filename);
+            end
+
+         
             if num_scans == 0
                 data_matrix = cat(1,data_matrix,(dum(scan_num,:)));
             else
+
                 data_matrix = cat(1,data_matrix,mean(dum(scan_num:scan_num+num_scans,:)));
             end
             scan_num = scan_num+num_scans;
         end
         clear dum
     end
-    data_matrix = cat(1,mz,data_matrix);
-    
+    data_matrix = cat(1, mz(:).', data_matrix);
     output = cat(2,end_scan,num2cell(data_matrix));
     output = cat(2,start_scan,output);
     output = cat(2,files,output);
     output = cat(2,labels,output);
-    % output = cat(2,labels2,output);
-    
-    writecell(output,'aligned_for_postprocessing.csv')   
-catch
+    writecell(output,'aligned_for_postprocessing.csv'); 
+ catch 
     warning('no preprocessed files found?');
 end
+
+
 % 
 % cells = unique(metadata(:,3));
 % GD = metadata(:,3);
@@ -61,3 +98,4 @@ end
 % GD(ind) = cellstr('negative');
 % 
 % output = cat(2,GD,output);
+%
